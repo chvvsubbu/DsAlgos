f6f65866b7395a25a00ad2f03b2579329d32653b
Subbu <subrahmanyeswararao.chekka@oracle.com>
2020-05-29 21:39:29 +0530

    slidingWindow: MaxFruitCountOf2Types

---
+++ src/patterns/slidingwindow/MaxFruitCountOf2Types.java
@@ -0,0 +1,34 @@
+package patterns.slidingwindow;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MaxFruitCountOf2Types {
+
+    public static int findLength(char[] arr) {
+        int windowEnd = 0, windowStart = 0;
+        int result = Integer.MIN_VALUE;
+        Map<Character, Integer> charMap = new HashMap<>();
+
+
+        for(windowEnd =0;windowEnd <arr.length; windowEnd++){
+
+            charMap.put(arr[windowEnd], charMap.getOrDefault(arr[windowEnd], 0)+1 );
+            while(charMap.size() >2){
+                charMap.put(arr[windowStart], charMap.get(arr[windowStart])-1);
+                if(charMap.get(arr[windowStart]) == 0) {
+                    charMap.remove(arr[windowStart]);
+                }
+                windowStart++;
+            }
+            result = Math.max(result,windowEnd-windowStart+1);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int count = findLength(new char[]{'A', 'B', 'C', 'A', 'C'});
+        System.out.println("count: "+ count );
+    }
+}
